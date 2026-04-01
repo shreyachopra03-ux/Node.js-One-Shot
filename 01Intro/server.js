d22c9909fad1528d86385fcaa8b6ca27b444682e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-01 17:16:23 +0530

    docs: appended file

---
+++ 01Intro/server.js
@@ -0,0 +1,40 @@
+// How NodeJS differs from vanilla JS ?
+// 1) Node runs on a server - not in a browser ( backend not frontend )
+// 2) The console is the terminal window
+console.log("Hello world");
+// 3) global object instead of window object
+// console.log(global);
+// 4) Has common core modules that we will explore
+// 5) CommonJS modules instead of ES6 modules
+// 6) Missing some API's like fetch
+
+const os = require('os');
+const path = require('path');
+// const math = require('./math');
+
+// or another way of doing the above thing is:
+const { add, subtract, multiply, divide } = require('./math');
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
+// console.log(path.parse(__dirname));
+
+// console.log(math.add(2, 3));
+// console.log(math.subtract(2, 3));
+// console.log(math.multiply(2, 3));
+// console.log(math.divide(2, 3));
